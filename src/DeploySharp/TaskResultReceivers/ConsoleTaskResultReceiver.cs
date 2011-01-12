363bda8d5b866eb1076f35ad1344494c06d85d54
Mario Pareja <pareja.mario@gmail.com>
2011-01-12 02:57:18 -0500

    Added a task result receiver that outputs to the console.

---
+++ src/DeploySharp/TaskResultReceivers/ConsoleTaskResultReceiver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using DeploySharp.Core;
+
+namespace DeploySharp.TaskResultReceivers
+{
+	public class ConsoleTaskResultReceiver : ITaskResultReceiver
+	{
+		public void ReceiveError(string message)
+		{
+			var colour = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine ("ERROR: " + message);
+			Console.ForegroundColor = colour;
+		}
+
+		public void ReceiveSuccess(string message)
+		{
+			Console.WriteLine (message);
+		}
+	}
+}
