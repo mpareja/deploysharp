7659d775c9ec2272748e132bd5ffe13b29c44c54
Mario Pareja <pareja.mario@gmail.com>
2011-03-17 00:01:30 -0400

    Added task for executing SQL script files against SQL Server.

---
+++ src/DeploySharp.Tests/Tasks/ExecuteSqlScriptsTaskTests.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using DeploySharp.Tasks.SqlServer;
+using NUnit.Framework;
+using Tests;
+
+namespace DeploySharp.Tests.Tasks
+{
+	[TestFixture]
+	public class ExecuteSqlScriptsTaskTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			_table = "my_table_" + DateTime.Now.Ticks;
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			using (var connection = GetConnection())
+			{
+				connection.Open();
+				using (var command = connection.CreateCommand ())
+				{
+					command.CommandType = CommandType.Text;
+					command.CommandText = string.Format(@"
+						if object_id('{0}') is not null
+							drop table {0}", _table);
+					command.ExecuteNonQuery ();
+				}
+			}
+		}
+
+		[Test]
+		public void can_execute_script()
+		{
+			var task = GetTaskWithCredentials();
+			using (var tempdir = new TempDirectoryPathAbsolute())
+			{
+				// write the script file to drive
+				var file = tempdir.SaveToTempFile(string.Format (@"
+					create table {0} ( my_id int );
+					go
+					insert into {0} values ( 2 );
+					go", _table));
+
+				task.ScriptFiles = new[] { file };
+
+				task.Prepare().AssertNoErrors ();
+				task.Execute().AssertNoErrors ();
+			}
+			AssertQueryReturns("select my_id from " + _table, 2);
+		}
+
+		[Test]
+		public void can_execute_multiple_scripts()
+		{
+			var task = GetTaskWithCredentials ();
+			using (var tempdir = new TempDirectoryPathAbsolute ())
+			{
+				var file1 = tempdir.SaveToTempFile (string.Format (@"
+					create table {0} ( my_id int );
+					go
+					insert into {0} values ( 1 );
+					go", _table));
+
+				var file2 = tempdir.SaveToTempFile(string.Format(@"
+					insert into {0} values ( 2 )", _table));
+
+				task.ScriptFiles = new[] { file1, file2 };
+
+				task.Prepare ().AssertNoErrors ();
+				task.Execute ().AssertNoErrors ();
+			}
+
+			using (var connection = GetConnection ())
+			{
+				connection.Open ();
+				using (var command = connection.CreateCommand ())
+				{
+					command.CommandType = CommandType.Text;
+					command.CommandText = "select my_id from " + _table + " where my_id = 1";
+					Assert.AreEqual (1, command.ExecuteScalar ());
+				}
+				using (var command = connection.CreateCommand ())
+				{
+					command.CommandType = CommandType.Text;
+					command.CommandText = "select my_id from " + _table + " where my_id = 2";
+					Assert.AreEqual (2, command.ExecuteScalar ());
+				}
+			}
+		}
+
+		private void AssertQueryReturns(string commandText, int expected)
+		{
+			using (var connection = GetConnection ())
+			{
+				connection.Open();
+				using (var command = connection.CreateCommand ())
+				{
+					command.CommandType = CommandType.Text;
+					command.CommandText = commandText;
+					Assert.AreEqual(expected, command.ExecuteScalar());
+				}
+			}
+		}
+
+		private SqlConnection GetConnection() 
+		{
+			var task = GetTaskWithCredentials();
+			var builder = new SqlConnectionStringBuilder ()
+			{
+				DataSource = task.Server,
+				InitialCatalog = task.Database,
+				UserID = task.Username,
+				Password = task.Password
+			};
+
+			return new SqlConnection (builder.ToString ());
+		}
+
+		private ExecuteSqlScriptsTask GetTaskWithCredentials()
+		{
+			return new ExecuteSqlScriptsTask {
+				Server = ConfigurationManager.AppSettings["testDbServer"],
+				Database = ConfigurationManager.AppSettings["testDbName"],
+				Username = ConfigurationManager.AppSettings["testDbUsername"],
+				Password = ConfigurationManager.AppSettings["testDbPassword"]
+			};
+		}
+
+		private string _table;
+	}
+}
---
+++ src/DeploySharp/Tasks/SqlServer/ExecuteSqlScriptTask.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+using DeploySharp.Core;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+
+namespace DeploySharp.Tasks.SqlServer
+{
+	public class ExecuteSqlScriptsTask : IExecutable, IPreparable, IDisposable
+	{
+		public string Server { get; set; }
+		public string Database { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public IEnumerable<string> ScriptFiles { get; set; }
+
+		public TaskResult Prepare()
+		{
+			var results = new TaskResult ();
+			if (_foundSmoAssembly == false)
+			{
+				results.Error ("!!!! IF SQL SERVER NOT INSTALLED, INSTALL SQL SERVER SMO REDISTRIBUTABLE !!!!");
+			}
+
+			foreach (var path in ScriptFiles)
+			{
+				try
+				{
+					// obviously not the most memory efficient - consider deferring
+					var script = File.ReadAllText (path, Encoding.Default);
+					_scripts.Enqueue(new ScriptFile {
+						Filename = path,
+						Script = script
+					});
+				}
+				catch(Exception e)
+				{
+					results.Error(e, "Error reading file: {0}", path);
+				}
+			}
+
+			_conn = OpenConnection (results);
+			return results;
+		}
+
+		public virtual TaskResult Execute()
+		{
+			var result = new TaskResult();
+			result.Success("Executing scripts on server '{0}' database '{1}'.", Server, Database);
+			foreach (var scriptFile in _scripts)
+			{
+				try
+				{
+					ExecuteSql(scriptFile.Script);
+					result.Success("\tExecuted script: {0}", scriptFile.Filename);
+				}
+				catch(Exception e)
+				{
+					var exception = e.InnerException ?? e;
+					result.Error("Error executing script: {0}:\n{1}",
+						scriptFile.Filename, exception.Message);
+				}
+			}
+			return result;
+		}
+
+		protected void ExecuteSql(string script)
+		{
+			ServerConnection.ConnectionContext.ExecuteNonQuery(script);
+		}
+
+		private Server ServerConnection
+		{
+			get
+			{
+				if (_server == null)
+					_server = new Server (new ServerConnection (_conn));
+				return _server;
+			}
+		}
+
+		private SqlConnection OpenConnection(TaskResult results)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = Server,
+                InitialCatalog = Database,
+                UserID = Username,
+                Password = Password
+            };
+
+            var conn = new SqlConnection (builder.ToString ());
+            try
+            {
+                conn.Open ();
+            	results.Success("Successfully opened connection with SQL Server.");
+            }
+            catch (Exception e)
+            {
+                results.Error (e, "Unable to open connection to SQL Server '{0}'"
+                    + " database '{1}'", Server, Database);
+            }
+            return conn;
+        }
+
+		public void Dispose()
+		{
+			if (_conn != null)
+			{
+				_conn.Dispose ();
+				_conn = null;
+			}
+		}
+
+		protected class ScriptFile
+		{
+			public string Filename;
+			public string Script;
+		}
+
+		/* An untested attempt at checking to make sure Microsoft SMOs are installed */
+		static ExecuteSqlScriptsTask()
+		{
+			AppDomain.CurrentDomain.AssemblyResolve += (s, args) =>
+			{
+				if (args.Name.ToUpperInvariant ().Contains ("SMO"))
+					_foundSmoAssembly = false;
+				return null;
+			};
+		}
+		private static bool _foundSmoAssembly = true;
+
+		protected Queue<ScriptFile> _scripts = new Queue<ScriptFile> ();
+		private Server _server;
+		private SqlConnection _conn;
+	}
+}
